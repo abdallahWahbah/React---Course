1a4fa70fd7c002db6e867e7ed42182900eef00bc
abdowahbah <abdowahbah123@yahoo.com>
2021-11-04 13:00:19 +0200

    section 3 & 4 & 5

--- 3/src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-// We don't render too many components here, we only render once for our root component
-ReactDOM.render(<App/>, document.getElementById("root"));
