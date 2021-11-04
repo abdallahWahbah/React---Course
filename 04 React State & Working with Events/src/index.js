7ef4382610189ef390e660cdd7eb00ef81cce279
abdowahbah <abdowahbah123@yahoo.com>
2021-11-04 12:56:49 +0200

    first commit

---
+++ 04 React State & Working with Events/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// We don't render too many components here, we only render once for our root component
+ReactDOM.render(<App/>, document.getElementById("root"));
---
+++ 05 Rendering Lists & Conditional Content/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,31 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+// import {useState} from 'react';
+
+// In React, we get only one parameter per component
+const ExpenseItem = (props)=>
+{
+    // const[title, setTitle] = useState(props.title);
+    // useState returns 2 things in an array: the value and a function to udpate the value. So we are using destructuring
+
+    // const clickHandler = () =>
+    // {
+    //     setTitle("Updated");
+    //     // by calling the function in the state, we update the value and more important:
+    //     // we call the function "ExpenseItem"
+    //     // again cause it is not called more than one time (initializing) by itself
+    // }
+    
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />   
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+            {/* <button onClick={clickHandler}>Cahnge title</button> */}
+        </Card>
+    )
+}
+export default ExpenseItem;
